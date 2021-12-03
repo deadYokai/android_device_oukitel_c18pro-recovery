82c3555f2e3199b0c7fd5094e7c8b4353f971c8e
MyNameIsKitsune <i.misterok21@gmail.com>
2021-12-03 18:14:38 +0200

    Update omni_c18pro.mk

--- omni_c18pro.mk
+++ omni_c18pro.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := device/oukitel/c18pro
 
 # Necessary to mount a PC
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) 
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk) 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk) 
 
 # $(call inherit-product, device/Oukitel/C18_Pro.mk)
 
@@ -13,7 +13,7 @@ PRODUCT_PACKAGES += \
     charger_res_images
 
     
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/oukitel/c18pro/root,recovery/root)
+# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/oukitel/c18pro/root,recovery/root)
 
 # Release name
 PRODUCT_RELEASE_NAME := C18_Pro
