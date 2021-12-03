e107a0ea2c1dd0b22481dd68b242330a9005fe70
MyNameIsKitsune <i.misterok21@gmail.com>
2021-12-03 22:34:38 +0200

    Update omni_c18pro.mk

--- omni_c18pro.mk
+++ omni_c18pro.mk
@@ -1,10 +1,9 @@
 LOCAL_PATH := device/oukitel/c18pro
 
-# Necessary to mount a PC
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk) 
-
-# $(call inherit-product, device/Oukitel/C18_Pro.mk)
+# Necessary to mount a PC.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, vendor/pb/config/common.mk)
 
@@ -14,8 +13,8 @@ PRODUCT_PACKAGES += \
 
     
 # PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/oukitel/c18pro/root,recovery/root)
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/root/init.recovery.mt6757.rc:root/init.recovery.mt6757.rc
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/root/sbin/permissive.sh:root/sbin/permissive.sh
+#PRODUCT_COPY_FILES += $(LOCAL_PATH)/root/init.recovery.mt6757.rc:root/init.recovery.mt6757.rc
+#PRODUCT_COPY_FILES += $(LOCAL_PATH)/root/sbin/permissive.sh:root/sbin/permissive.sh
 
 # Release name
 PRODUCT_RELEASE_NAME := C18_Pro
