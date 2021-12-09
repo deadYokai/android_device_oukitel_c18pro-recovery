8e516485704b9a44d8795df6c7a19bbe2b69d1a9
MyNameIsKitsune <i.misterok21@gmail.com>
2021-12-09 13:44:33 +0200

    Update omni_c18pro.mk

--- omni_c18pro.mk
+++ omni_c18pro.mk
@@ -5,8 +5,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, vendor/pb/config/common.mk)
-
+#$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_PACKAGES += \
     charger_res_images
