108aa56b71a62a0440630bf9343ff9b2b159238a
MyNameIsKitsune <i.misterok21@gmail.com>
2022-02-15 09:28:33 +0200

    Update omni_c18pro.mk

--- omni_c18pro.mk
+++ omni_c18pro.mk
@@ -2,10 +2,10 @@ LOCAL_PATH := device/oukitel/c18pro
 
 # Necessary to mount a PC.
 
-$(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+
 PRODUCT_DEVICE := c18pro
 PRODUCT_NAME := omni_c18pro
 PRODUCT_BRAND := oukitel
