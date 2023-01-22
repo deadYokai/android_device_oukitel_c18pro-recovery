7047434ebb8e4f33c1b889bb5451ae55873fc694
MyNameIsKitsune <mynameiskitsune@proton.me>
2023-01-22 18:26:28 +0200

    Fix conflict

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,11 +17,12 @@ LOCAL_SRC_FILES    := etc/init.recovery.mt6757.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
+# Confilcts with system/core/rootdir
+# include $(CLEAR_VARS)
+# LOCAL_MODULE       := init.rc
+# LOCAL_MODULE_TAGS  := optional
+# LOCAL_MODULE_CLASS := ETC
+# LOCAL_SRC_FILES    := etc/init.rc
+# LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+# include $(BUILD_PREBUILT)
 
