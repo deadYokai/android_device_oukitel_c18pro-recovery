a25c3c3368f7a38c1bd922737c14cc905ea56aa6
MyNameIsKitsune <mynameiskitsune@proton.me>
2023-01-24 01:25:56 +0200

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,21 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 
 # Recovery init configuration files
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
+# include $(CLEAR_VARS)
+# LOCAL_MODULE       := ueventd.rc
+# LOCAL_MODULE_TAGS  := optional
+# LOCAL_MODULE_CLASS := ETC
+# LOCAL_SRC_FILES    := etc/ueventd.rc
+# LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+# include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.mt6757.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.mt6757.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
+# include $(CLEAR_VARS)
+# LOCAL_MODULE       := init.recovery.mt6757.rc
+# LOCAL_MODULE_TAGS  := optional
+# LOCAL_MODULE_CLASS := ETC
+# LOCAL_SRC_FILES    := etc/init.recovery.mt6757.rc
+# LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+# include $(BUILD_PREBUILT)
 
 # Confilcts with system/core/rootdir
 # include $(CLEAR_VARS)
